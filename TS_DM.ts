0a7cfd21744638b607b702b6117104025602de6d
Abu Al Shahriar Rifat <79824559+2019005005-Abu@users.noreply.github.com>
2023-08-13 20:58:03 +0600

    Add files via upload

---
+++ TS_DM.ts
@@ -0,0 +1,8 @@
+//The DOM
+//Document Object Model
+let img=document.querySelector('img')!
+let myImg=document.getElementById('#img') ! as HTMLImageElement;
+let  nextImage=<HTMLImageElement> document.getElementById('#img');
+img.src
+myImg.src
+nextImage.src
